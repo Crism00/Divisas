734e1d2fa9f029ca52a5191a985a939d3ac71e5e
Cristian <crismoo3012@gmail.com>
2024-10-24 19:35:50 -0600

    No funciona pipip

--- Divisas/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Divisas.AssemblyInfo.cs
+++ Divisas/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Divisas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Divisas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a80b1af051d7622670451d86a30226107f090a25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+248d9c9127f7af7b996fe13ae303ad9edd5516d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Divisas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Divisas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
